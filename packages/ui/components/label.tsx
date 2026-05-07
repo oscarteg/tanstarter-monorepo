dc898c80db005c4b0c901347c1d68676f424561c
Nathaniel John Tampus <nate@mugnavo.com>
2026-05-08 07:05:29 +0800

    chore: update oxlint jsx-a11y rule reference

--- packages/ui/components/label.tsx
+++ packages/ui/components/label.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
-    // oxlint-disable-next-line jsx_a11y/label-has-associated-control
+    // oxlint-disable-next-line jsx-a11y/label-has-associated-control
     <label
       data-slot="label"
       className={cn(
